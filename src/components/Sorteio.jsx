eca52c35e512aa4138db63969fc001e60fbbc829
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-25 09:52:37 -0300

    UPDATE: created and inser content

---
+++ src/components/Sorteio.jsx
@@ -0,0 +1,17 @@
+import './Sorteio.css'
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+    return (
+
+        <Card title='Sorteio de um número' purple>
+            <div >
+                <span>
+                    <span>Resultado:</span>
+                    <strong>8</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
