ca63e4a44bc7edc854d75dcd4a79366eafb841df
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-25 11:38:14 -0300

    FIX: corrects parameter name

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -3,15 +3,15 @@ import { createStore,combineReducers } from "redux";
 
 const reducers = combineReducers({
     numeros:function(state,action){
-        console.log(state, ' ',action)
+        //console.log(state, ' ',action)
         return{
-            min:1,
-            mas:10
+            min:7,
+            max:31
         }
     },
     nomes:function(state,action){
-        console.log(state, ' ',action)
-        return ['Miguelito', 'Lorem', 'Ipsum']
+        //console.log(state, ' ',action)
+        return ['Miguelito', 'Bia', 'Miguel']
     }
 })
 
